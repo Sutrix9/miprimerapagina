b4119db7a8517e6960ba5e8ff35125434ca9cd30
Sutrix9 <matiasvelez2104@gmail.com>
2023-05-22 22:12:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick() {
+    alert('¡Hola! has hecho clic en el boton.');
+}
+miBoton.addEventListener("click", function () {
+    alert("haz hecho click en mi");
+});
